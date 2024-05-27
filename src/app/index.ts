653ec4c0df69661ddb25ea8b604bd22a5616b5d5
kamal-y <kamalyadavand@gmail.com>
2024-05-27 10:28:39 +0530

    27-05

--- src/app/index.ts
+++ src/app/index.ts
@@ -18,9 +18,9 @@ export async function initServer() {
   app.use(bodyParser.json());
   app.use(cors());
 
-  app.get("/", (req, res) => {
-    res.status(200).json({ message: "Everything is good" });
-  });
+    app.get("/", (req, res) => {
+      res.status(200).json({ message: "Everything is good" });
+    });
 
   const graphqlServer = new ApolloServer<GraphqlContext>({
     typeDefs: `
