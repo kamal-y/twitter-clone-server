517b9b5dcb983012448bf0cd0ee5153498299d14
Kamal Yadav <kamalyadavand@gmail.com>
2024-12-04 08:36:25 +0530

    Update redis.ts

--- src/clients/redis/redis.ts
+++ src/clients/redis/redis.ts
@@ -1,4 +1,4 @@
 import Redis from "ioredis"
 
-export const redisClient = new Redis("rediss://default:Aca1AAIncDE4MzI5MTVkZTQzY2Q0MjEwOWNmZDllYWQxOTJjMjA4OHAxNTA4Njk@glorious-heron-50869.upstash.io:6379");
+export const redisClient = new Redis("rediss://default:Ab-zAAIjcDFjZGQzZmRhMjdjYzM0MTRiOTkwNjg4MGY4ZTVhNDQ1M3AxMA@open-liger-49075.upstash.io:6379");
 
